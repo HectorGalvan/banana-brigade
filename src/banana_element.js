722d9d4f64b5e3a18591562e61437fd388b9ccfc
Andrew Brown <andrew@monsterboxpro.com>
2021-02-28 12:39:22 -0500

    banana should link to hash node profile

--- src/banana_element.js
+++ src/banana_element.js
@@ -7,17 +7,18 @@ export default class BananaElement {
 
   static async render(banana_id,hashnode_username,cognito_uuid,vector){
     console.debug('render', arguments)
-    const new_banana = document.createElement("div")
+    const new_banana = document.createElement("a")
+    new_banana.href = `https://hashnode.com/@${hashnode_username}`
+    new_banana.target = '_blank'
     new_banana.classList.add('banana')
     new_banana.style.top  = `${vector.y}px`
     new_banana.style.left = `${vector.x}px`
     new_banana.dataset.cognito_uuid = cognito_uuid
     new_banana.dataset.banana_id    = banana_id
-    //new_banana.dataset["banana_id"] = banana_id
 
-    const label = document.createElement("div")
+    const label = document.createElement("span")
     label.classList.add('hashnode_username')
-    label.innerText = "@" + hashnode_username
+    label.innerText = `@${hashnode_username}`
 
     new_banana.appendChild(label)
 
